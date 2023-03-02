5aa7f6c209648da21567f34b56fce01eef8357ac
Nelson Pang <nelsonnelpang@yahoo.com>
2023-03-02 17:40:31 +0800

    Initialize Spring Boot Project

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+// @Configuration: Tags the class as a source of bean definitions for the application context.
+// @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings
+// @ComponentScan: Tells Spring to look for other components, configurations, and services in the com.example package
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	// print all beans
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+}
