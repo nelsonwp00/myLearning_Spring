8d0671b908813e8f3647b09cd7fe162a4469a19b
Nelson Pang <nelsonnelpang@yahoo.com>
2023-03-03 21:44:08 +0800

    Add Routing Support

--- src/main/java/com/example/demo/BranchAController.java
+++ src/main/java/com/example/demo/BranchAController.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
-
+@RequestMapping("/branchA")
+public class BranchAController {
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String base() {
+        return "branch A";
     }
 }
