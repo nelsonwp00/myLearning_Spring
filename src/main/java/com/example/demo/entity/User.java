0d5cd78733a9b5bddb66c9075d4c003b2136fbf4
Nelson Pang <nelsonnelpang@yahoo.com>
2023-03-08 20:56:39 +0800

    Add Controller Input Validation

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,15 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
+    @NotEmpty(message = "Name may not be empty")
     private String name;
-
+    @NotEmpty(message = "Email may not be empty")
     private String email;
 
     public Integer getId() {
